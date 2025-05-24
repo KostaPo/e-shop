91e81e80ce15f34452e9131dccac823cfd12687c
Migel Torrez <esley.burach@magneticoak.com>
2025-05-24 22:19:49 +0300

    add mvp api gateway

---
+++ cloud-api-gateway/src/main/java/biz/xcompany/gateway/ApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package biz.xcompany.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] arguments) {
+        SpringApplication.run(ApiGatewayApplication.class, arguments);
+    }
+}
